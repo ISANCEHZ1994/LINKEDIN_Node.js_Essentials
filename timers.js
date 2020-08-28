5059e770719551366ab08f756a5f9e26c25c06c2
Israel <Israel.pro1994@gmail.com>
2020-08-28 14:26:11 -0500

    made a timer interval

--- timers.js
+++ timers.js
@@ -1,7 +1,49 @@
-const waitTime = 3000;
+// const waitTime = 5000; // 1000 miliseconds is 1 second
+// const waitInterval = 500; // half a second
+// let currentTime = 0; //
 
-console.log(`setting a ${waitTime/1000} second delay`);
+// const incTime = () => {
+//     currentTime += waitInterval;
+//     const p = Math.floor((currentTime / waitTime) * 100);
+//     process.stdout.clearLine();
+//     process.stdout.cursorTo(0);
+//     process.stdout.write(`waiting ... ${p}%`)
+//     // console.log(`waiting time: ${currentTime/1000} seconds`);
+// }
 
-const timerFinished = () => console.log("done");
+// console.log(`setting a ${waitTime/1000} second delay`);
 
+// const timerFinished = () => {
+//     clearInterval(interval)
+//     console.log("done")
+// };
+
+// const interval = setTimeout(incTime, waitInterval);
+// setTimeout(timerFinished, waitTime);
+
+
+const waitTime = 5000; // 1000 miliseconds is 1 second
+const waitInterval = 500; // half a second
+let currentTime = 0; 
+
+const incTime = () => {
+  currentTime += waitInterval;
+  const p = Math.floor((currentTime / waitTime) * 100);
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0);
+  process.stdout.write(`waiting ... ${p}%`);
+};
+
+console.log(`setting a ${waitTime / 1000} second delay`);
+
+const timerFinished = () => {
+  clearInterval(interval);
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0);
+  console.log("done");
+};
+
+const interval = setInterval(incTime, waitInterval);
 setTimeout(timerFinished, waitTime);
+
+// Note to self: for some reason my code was not working like his and I can find out why - downloaded and copied the work 
