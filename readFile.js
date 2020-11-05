6babd820fa27f1869784fe583878960196896a29
Israel <Israel.pro1994@gmail.com>
2020-11-05 17:59:19 -0600

    learned about writeFile

--- readFile.js
+++ readFile.js
@@ -1,4 +1,15 @@
 const fs = require('fs');
 
-const text = fs.readFileSync("./assets/Readme.md", "utf-8");
-console.log(text);
+fs.readFile("./assets/Readme.md", "utf-8", (err,text) => {
+
+    if(err){
+        console.log(`An error has occured: ${err.message}`);
+        process.exit();
+    }
+    console.log("file contents read");
+    console.log(text);
+    
+});
+
+// NOTE: If you remove utf-8 you will recive binary code in the terminal
+
