4e79fc3818a3f6266f5aa258480d7ec900197796
Israel <Israel.pro1994@gmail.com>
2020-11-05 18:14:56 -0600

    directory finished

--- directory.js
+++ directory.js
@@ -1,8 +1,16 @@
 const fs = require('fs');
 
-fs.mkdir("storage-files", err => {
+if(fs.existsSync("storage-files")){ // This is to check if the directory already exist
+    console.log("Already Exists!")
+}else{
+    fs.mkdir("storage-files", err => {
     if(err){
         throw err
     }
     console.log("directory created");
-});
+    });
+};
+
+
+
+// NOTE: If the directory is ALREADY created then it will throw and error
